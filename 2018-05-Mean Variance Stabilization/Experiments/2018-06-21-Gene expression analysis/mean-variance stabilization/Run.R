c993fb227b9ec5934f0d10315841c118a2137eac
faezeh-bayat <39104003+faezeh-bayat@users.noreply.github.com>
2018-06-26 02:08:46 -0700

    Update Run.R

--- 2018-05-Mean Variance Stabilization/Experiments/2018-06-21-Gene expression analysis/mean-variance stabilization/Run.R
+++ 2018-05-Mean Variance Stabilization/Experiments/2018-06-21-Gene expression analysis/mean-variance stabilization/Run.R
@@ -1,4 +1,4 @@
-setwd("/Users/faezehbayat/Documents/Experiments/2018-06-21-Gene expression analysis/mean-variance stabilization")
+setwd("Projects/2018-05-Mean Variance Stabilization/Experiments/2018-06-21-Gene expression analysis/mean-variance stabilization/Run.R")
 rm(list=ls())
 library(pracma)
 library(readxl)
