eac8568c69e4a8981ccc2189ff7cc9b0113cf4e6
faezeh-bayat <39104003+faezeh-bayat@users.noreply.github.com>
2018-10-29 12:14:32 -0700

    Update script.R

--- 2018-05-Mean Variance Stabilization/Paper/paper/gene expression analysis comparison/script.R
+++ 2018-05-Mean Variance Stabilization/Paper/paper/gene expression analysis comparison/script.R
@@ -1,1 +1,17 @@
 
+setwd("/Users/faezehbayat/Documents/new experiments/gene expression analysis")
+rm(list=ls())
+library(ggplot2)
+data <- read.csv("total results.csv")
+ggplot(data,aes(x=Dataset,y=Pearson.Correlation,fill=Method))+geom_bar(position = "dodge2")
+x=data$Dataset
+y=data$Pearson.Correlation
+grp=data$Method
+
+
+ggplot(data, aes(x, y, group = grp)) + geom_col(aes(fill = grp), position = "dodge2") +theme_bw()
+ggplot(data, aes(x, y, group = grp)) + geom_col(aes(fill = grp), position = "dodge2") +theme_bw()+
+scale_fill_manual("legend", values = c("Variance stabilization" = "red", "Fold enrichment" = "orange", "log(fold enrichment)" = "blue", "log(p-value)"="pink","P-value"="green"))
+ggplot(data, aes(x, y, group = grp)) + geom_col(aes(fill = grp), position = "dodge2") +theme_bw()+
+scale_fill_brewer(palette="Dark2")+labs(x = "\n\nChIP-seq datasets",y="\nPearson correlation")
+
